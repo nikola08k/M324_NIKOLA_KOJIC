2a47c748dcf14b49ac922c2c55e3570f82b19af3
Nikola Kojic <nikola.kojic@lehre.roche.com>
2025-12-12 11:37:37 +0100

    Aufgabe 6

---
+++ Task6/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS test_table (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO test_table (name) VALUES
+('Nikola'),
+('M324'),
+('Docker Compose');
