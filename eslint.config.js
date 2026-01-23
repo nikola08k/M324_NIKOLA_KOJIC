fadf1fcbba1b430cfc65e44c84de4cad8ef57142
Nikola Kojic <nikola.kojic@lehre.roche.com>
2026-01-23 10:35:02 +0100

    some stuff

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      eqeqeq: 'error',
+      'no-var': 'error',
+    },
+  },
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+      },
+    },
+  },
+]
